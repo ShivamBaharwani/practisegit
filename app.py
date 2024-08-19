320df250a76e628fbb98d416b3adb4684f8176db
ShivamBaharwani <youremail@example.com>
2024-08-19 17:29:30 +0530

    substraction updated

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ def add(num1,num2):
 def division(num1,num2):
     return num1/num2
 
-def substract(num1,num2):
-    return num1-num2
+def substract(num1,num2,num3):
+    return num1-num2-num3
     
 def multiply(num1,num2):
     return num1*num2
