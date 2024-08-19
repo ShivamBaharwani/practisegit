49f1eb938d7293046d33a9f817bd624e555f6af1
Shivam Baharwani <104077242+ShivamBaharwani@users.noreply.github.com>
2024-08-19 17:34:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ def add(num1,num2,num3):
 def division(num1,num2):
     return num1/num2
 
-def substract(num1,num2,num3):
+def substract(num1,num2,num4):
     return num1-num2-num3
     
 def multiply(num1,num2):
