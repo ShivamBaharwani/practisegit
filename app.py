546426fdd1dc4912f933bd2800944263617f791a
ShivamBaharwani <youremail@example.com>
2024-08-19 17:36:04 +0530

    num3-num4

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ def add(num1,num2,num3):
 def division(num1,num2):
     return num1/num2
 
-def substract(num1,num2,num3):
+def substract(num1,num2,num5):
     return num1-num2-num3
     
 def multiply(num1,num2):
